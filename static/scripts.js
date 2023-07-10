4b11c3382c5cea5ed95019d480b92fa0a2d3b635
Apopcalyptic <meri@apopcalyptic.co.uk>
2023-07-10 13:12:02 +0100

    Lots of commits!

--- static/scripts.js
+++ static/scripts.js
@@ -7,3 +7,50 @@ var books = [
    
  
 ];
+
+//function fillReview is used by review.html to populate the review page. 'Book' is the position in the array of the book referred to, and is embedded in the URL. 
+function fillReview()
+{
+     //extract the parsed book variable from the URL to creat the book variable
+     const queryString = window.location.search;
+     const urlParams = new URLSearchParams(queryString);
+     var book = urlParams.get('book');
+     //--debug code -- console.log(book);   
+
+    //some logic here to check if a paramater wasn't parsed in the URL. If it wasn't, check the length of the Books array, and randomly land on a reviw  (FIX LATER)
+    if (book == null) {
+        var randomMax = books.length; // - Fix this later, for now, if unset, set book to 0
+        book = Math.floor(Math.random() * randomMax);
+        
+    }
+
+    
+    //first block of code gathers to text elements to be populated from the HTML
+    var title = document.getElementById("reviewTitle");
+    var author = document.getElementById("reviewAuthor");
+    var synopsis = document.getElementById("reviewSynopsis");
+    var body = document.getElementById("reviewBody");
+    var image = document.getElementById("reviewImage");
+    
+    //this extracts the relevant entry for each elemt from the array
+    var toTitle =  books[book][1];
+    var toAuthor = books[book][2];
+    var toSynopsis = books[book][3];
+    var toBody = books[book][4];
+    //then changes the innerHTML property of each elemtent to output the data. 
+    title.innerHTML = toTitle;
+    author.innerHTML = toAuthor;
+    synopsis.innerHTML = toSynopsis;
+    body.innerHTML = toBody;
+
+    //now we need some magic to build the image URL - images should be named to reflect the unique ID of each entry (the number at the start of the array entry)
+
+    var toImageURL = "./static/images/" + books[book][0] + ".jpg";
+    var toImageAlt = toTitle + " cover image";
+
+    image.setAttribute("src", toImageURL);
+    image.setAttribute("alt", toImageAlt);
+
+
+
+}
